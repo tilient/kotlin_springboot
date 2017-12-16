817e3b405d37b9b06a155e35dfa42ec94cd56162
wiffel <wiffel@tilient.net>
2017-12-16 19:04:58 +0100

    v0.03

--- src/main/kotlin/org/jetbrains/kotlin/demo/Application.kt
+++ src/main/kotlin/org/jetbrains/kotlin/demo/Application.kt
@@ -30,7 +30,7 @@ class GreetingController
 @SpringBootApplication
 class Application
 
-fun main(args: Array<String>)ld
+fun main(args: Array<String>)
 {
   SpringApplication.run(Application::class.java, *args)
 }
