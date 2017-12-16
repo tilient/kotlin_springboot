d83dc3344e601b694d6002701fa6c1bc94999b87
wiffel <wiffel@tilient.net>
2017-12-16 19:02:31 +0100

    v0.01

--- src/main/kotlin/org/jetbrains/kotlin/demo/Application.kt
+++ src/main/kotlin/org/jetbrains/kotlin/demo/Application.kt
@@ -2,10 +2,38 @@ package org.jetbrains.kotlin.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+// --- Data -------------------------------------------------------
+
+data class Greeting(val id: Long, val content: String)
+
+// --- Controller -------------------------------------------------
+
+@RestController
+class GreetingController
+{
+  val counter = AtomicLong()
+
+  @GetMapping("/greeting")
+  fun greeting(@RequestParam(
+                 value = "name", defaultValue = "World")
+               name: String) =
+    Greeting(counter.incrementAndGet(), "Hello, $name")
+
+}
+
+// --- Main -------------------------------------------------------
 
 @SpringBootApplication
 class Application
 
-fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+fun main(args: Array<String>)ld
+{
+  SpringApplication.run(Application::class.java, *args)
 }
+
+// ----------------------------------------------------------------
